af47a72fe248bd85b7db6ed9fb02a850410c4cee
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-03-19 00:40:24 -0300

    creating first component

---
+++ components/Header/index.tsx
@@ -0,0 +1,7 @@
+import styles from './Home.module.css'
+
+export default function Header() {
+  return (
+    <h2>dasd</h2>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
+import styles from './Home.module.css'
+
+// Components
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <Header />
   )
 }
